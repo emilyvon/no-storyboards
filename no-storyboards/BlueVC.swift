8e7bfef32c201d9265ad86f829ab67140a6304b6
emilyvon <emilyfung86@hotmail.com>
2016-02-23 23:43:26 +1030

    initial commit

---
+++ no-storyboards/BlueVC.swift
@@ -0,0 +1,59 @@
+//
+//  BlueVC.swift
+//  no-storyboards
+//
+//  Created by Mengying Feng on 28/01/2016.
+//  Copyright © 2016 Mengying Feng. All rights reserved.
+//
+
+import UIKit
+
+class BlueVC: UIViewController {
+
+    @IBOutlet weak var printLbl: UILabel!
+    var printTxt = ""
+    
+    convenience init(printMe: String) {
+        self.init(nibName: "BlueVC", bundle: nil)
+        printTxt = printMe
+        
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        printLbl.text = printTxt
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
